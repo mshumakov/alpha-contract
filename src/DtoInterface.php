60abaa96add07db2a96632521ee8f172782ee7b4
Maksim Shumakov <ms.profile.dev@gmail.com>
2020-06-19 18:32:29 +0500

    Add first interfaces.
    p.s.: Dto and Request.

---
+++ src/DtoInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace MSdev\Helper\AlphaContract;
+
+/**
+ * Interface DtoInterface
+ * @package MSdev\Helper\AlphaContract
+ */
+interface DtoInterface
+{
+}
---
+++ src/RequestInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace MSdev\Helper\AlphaContract;
+
+/**
+ * For multi-parameter query methods.
+ *
+ * Interface RequestInterface
+ * @package MSdev\Helper\AlphaContract
+ */
+interface RequestInterface extends DtoInterface
+{
+}
